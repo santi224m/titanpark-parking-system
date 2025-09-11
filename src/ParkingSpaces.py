66c9aa50396e93a442be3ee0a14f425dff71f749
Santiago Mondragon <santiago.mondragon224@gmail.com>
2025-09-10 20:54:39 -0700

    Include total spaces and percent full in parking availability api

--- src/ParkingSpaces.py
+++ src/ParkingSpaces.py
@@ -15,10 +15,15 @@ class ParkingSpaces:
     table = soup.find_all('table', {"class": "AvailabilityTable"})[0]
     all_tr = table.find_all('tr')
 
-    # We don't want to fetch prices for all parking spots
-    exclude_locs = set(['Fullerton Free Church'])
+    # We don't want to fetch prices for all parking structures
+    exclude_locs = set(['Fullerton Free Church', 'S8 and S10'])
 
-    parking_dict = {}
+    parking_dict = {
+      'Nutwood Structure': {'available': None, 'total': 2504, 'perc_full': None},
+      'State College Structure': {'available': None, 'total': 1373, 'perc_full': None},
+      'Eastside North': {'available': None, 'total': 1880, 'perc_full': None},
+      'Eastside South': {'available': None, 'total': 1242, 'perc_full': None},
+    }
     # Iterate through rows in table
     for tr in all_tr:
       # Get parking structure name
@@ -41,8 +46,9 @@ class ParkingSpaces:
       avail_count = int(avail_count)
 
       # Valid parking spot if this point is reached
-      parking_dict[struct_name] = avail_count
-    
+      parking_dict[struct_name]['available'] = avail_count
+      parking_dict[struct_name]['perc_full'] = round(1 - (avail_count / parking_dict[struct_name]['total']), 2)
+
     return parking_dict
 
 if __name__ == "__main__":
